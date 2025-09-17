4a5eea4489c8a1330ca15fc97baf455246daf18c
Eduardo Lombardo Fuzato <128429561+edu1107zera@users.noreply.github.com>
2025-09-17 14:33:06 -0300

    edu

--- src/controllers/exam.js
+++ src/controllers/exam.js
@@ -29,6 +29,12 @@ export const ExamController = {
         }
     },
     async index(req,res,next){
+        let query ={}
+        if (req.query.recordId) query = {recordId: Number(req.query.recordId)}
+        if (req.query.type) query = {type: req.query.type}
+        if (req.query.date) query = {date: new Date(req.query.date)}
+        if (req.query.result) query = {result: req.query.result}
+        if (req.query.observation) query = {observation: req.query.observation}
         const exams = await prisma.exam.findMany()
         res.status(200).json(exams)
     }
