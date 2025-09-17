89dc7a85857e7c49c847d79e10d1a9206ae6e095
Edu <educoppi8@gmail.com>
2025-09-17 17:18:09 -0300

    edu coppi

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -39,5 +39,35 @@ export const UserController = {
         })
 
         res.status(200).json(users)
+    },
+    async show(req, res, _next){
+        try{
+        const id = Number(req.params.id);  //PRECISA DO NUMBER PQ O JSON TRANSFORMA TUDO EM STRING
+
+        // funções assincronas precisam do await
+        const u = await prisma.user.findFirstOrThrow({
+            where: { id }
+        }); //função para encontrar o primeiro user com a id especificada, se não encontrar retorna um erro
+
+        res.status(200).json(u);
+
+        } catch (err){
+            res.status(404).json({error: "Usuário não encontrado"});
+        }
+    },
+    async delete(req, res, _next){
+        try{
+            const id = Number(req.params.id);  //PRECISA DO NUMBER PQ O JSON TRANSFORMA TUDO EM STRING
+    
+            // funções assincronas precisam do await
+            const u = await prisma.user.delete({
+                where: { id }
+            }); //função para encontrar o primeiro user com a id especificada, se não encontrar retorna um erro
+    
+            res.status(200).json(u);
+    
+            } catch (err){
+                res.status(404).json({error: "Usuário não encontrado"});
+            }
     }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,5 +5,6 @@ const route = Router();
 
 route.post('/', UserController.store);
 route.get('/', UserController.index);
-
+route.get('/:id', UserController.show); // ":id" é o nome do parametro (req.params.id). ".show" é o novo metodo criado no controller
+route.delete('/:id', UserController.delete);
 export default route;
