5553940bc23919094b9222b9dbfe9ddc60cb8b14
SARA MALAVAZI <139522059+saramalavazi94@users.noreply.github.com>
2025-09-25 17:17:22 -0300

    record sara

--- src/controllers/record.js
+++ src/controllers/record.js
@@ -5,26 +5,40 @@ export const RecordController = {
     //c - CREATE,INSERT,POST,SET,STORE
     async store(req, res, next) {
         try {
-            const { patientId, appointmentDate, anotacao } = req.body;
+            const { patientId, appointmentDate, anotacao, userId} = req.body;
+
             let p = await prisma.patient.findFirst({
                 where: { id: Number(patientId) }
-
-
-
             });
             if(!p){
                 res.status(301).json({ error: "Paciente não encontrado" })
                 return
             }
+            if(p.role != "PATIENT"){
+                res.status(401).json({error: "Não é um paciente"})
+            }
             
+            let u = await prisma.user.findFirst({
+                where: { id: Number(userId) }
+            });
+            if(!u){
+                res.status(301).json({error: "Usuário não encontrado"})
+                return
+            }
+            if(u.role == "PATIENT"){
+                res.status(401).json({error: "Usuário não pode ser igual a um paciente"})
+            }
+
                 const r = await prisma.record.create({
                 data: {
                     patientId: Number(patientId),
                     appointmentDate: new Date(appointmentDate),
-                    anotacao:anotacao
+                    anotacao:anotacao, 
+                    userId: Number(userId)
                 }
                     
             });
+
             
 
             res.status(201).json(r);
