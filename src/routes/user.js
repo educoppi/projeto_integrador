5b4d38f7bc2be3c119dfbe425aa4406f8d6d8b3c
Edu <educoppi8@gmail.com>
2025-09-30 16:06:50 -0300

    token coisas

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 import {Router} from 'express';
 import {UserController} from '../controllers/user.js' 
+import { verificaToken } from '../middlewares/auth.js';
 
 const route = Router();
 
@@ -7,6 +8,6 @@ route.post('/', UserController.store);
 route.post('/login', UserController.login);
 route.get('/', UserController.index);
 route.get('/:id', UserController.show); // ":id" é o nome do parametro (req.params.id). ".show" é o novo metodo criado no controller
-route.delete('/:id', UserController.delete);
-route.put('/:id', UserController.update);
+route.delete('/:id', verificaToken, UserController.delete);
+route.put('/:id', verificaToken, UserController.update);
 export default route;
