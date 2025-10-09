c2665f9360112deac0f868907b25904e4607efb1
Rafael Crempe <rafaelcrempe@gmail.com>
2025-10-09 12:06:45 -0300

    removi o verificatoken que tava dando erro

--- src/routes/movement.js
+++ src/routes/movement.js
@@ -3,10 +3,10 @@ import {MovementController} from '../controllers/movement.js'
 
 const route = Router();
 
-route.post('/',verificaToken, verificaRole('PHARMACY'), MovementController.store);
-route.get('/',verificaToken, verificaRole('PHARMACY'), MovementController.index);
-route.get('/:id',verificaToken, verificaRole('PHARMACY'), MovementController.show);
-route.delete('/:id', verificaToken, verificaRole('PHARMACY'), MovementController.delete);
-route.put('/:id',verificaToken, verificaRole('PHARMACY'), MovementController.update);
+route.post('/', MovementController.store);
+route.get('/', MovementController.index);
+route.get('/:id', MovementController.show);
+route.delete('/:id',  MovementController.delete);
+route.put('/:id', MovementController.update);
 
 export default route;
