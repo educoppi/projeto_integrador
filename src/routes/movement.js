26fad9a848a5da922409f79d3a69c355766bf8ed
Rafael Crempe <rafaelcrempe@gmail.com>
2025-11-07 15:03:45 -0300

    coloquei novas rotas de movimento

--- src/routes/movement.js
+++ src/routes/movement.js
@@ -3,7 +3,8 @@ import {MovementController} from '../controllers/movement.js'
 
 const route = Router();
 
-route.post('/', MovementController.store);
+route.post('/', MovementController.storeFarmacia);
+route.post('/', MovementController.storeDoctor);
 route.get('/', MovementController.index);
 route.get('/:id', MovementController.show);
 route.delete('/:id',  MovementController.delete);
